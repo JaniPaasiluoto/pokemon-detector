ea3880fedf268fc4c9b24bd3dd24550deaa79c24
Jani Paasiluoto <jani.paasiluoto@holda.fi>
2021-03-22 21:37:24 +0200

    read a frame from web cam

--- detector.py
+++ detector.py
@@ -1,13 +1,21 @@
+import time
+
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
 import imutils
 import pickle
 import cv2
+from imutils.video import VideoStream
 
-image = cv2.imread("examples/ivysaur.png")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+print("[INFO] taking pic in 3...")
+time.sleep(3.0)
+frame = vs.read()
+image = imutils.resize(frame, width=260)
 output = image.copy()
-
 image = cv2.resize(image, (96, 96))
 image = image.astype("float") / 255.0
 image = img_to_array(image)
@@ -17,6 +25,7 @@ print("[INFO] loading network...")
 model = load_model("assets/pokedex.model")
 print("[INFO] loading labels...")
 lb = pickle.loads(open("assets/lb.pickle", "rb").read())
+
 
 # classify the input image
 print("[INFO] classifying image...")
@@ -30,7 +39,9 @@ output = imutils.resize(output, width=400)
 cv2.putText(output, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
             0.7, (0, 255, 0), 2)
 
+cv2.imwrite("results/" + label + ".jpg", output)
+
 # show the output image
 print("[INFO] {}".format(label))
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+# cv2.imshow("Output", output)
+# cv2.waitKey(0)
